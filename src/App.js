b7eee549c4af7f1c63a891ea34bf9ee712bb640a
mel picardal <meljun.one@gmail.com>
2021-10-04 13:48:15 +0900

    learned Hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet name="Pepper" animal="Bird" breed="Cocaktiel"></Pet>
-      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier"></Pet>
+      <SearchParams />
     </div>
   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
